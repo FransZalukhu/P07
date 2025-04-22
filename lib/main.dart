a071641c5b66a150df46fd2bed6d0eb287daf69e
FransZalukhu <fransdamai2020@gmail.com>
2025-04-22 16:57:15 +0700

    Row & Column in UI

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,12 @@ import 'package:google_fonts/google_fonts.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Demo Layout',
+      title: 'Implementasi Row & Column',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -18,102 +20,78 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
+  final String judul = "Politeknik Negeri Bengkalis";
+  final String lokasi = "Bengkalis, Riau";
+  final String deskripsi = """
+    Politeknik Negeri Bengkalis (POLBENG) adalah satu-satunya politeknik negeri yang berada di Riau. 
+    Pada tanggal 29 Juli 2011, Politeknik Bengkalis resmi menjadi PTN dengan nama Politeknik Negeri Bengkalis melalui Peraturan Menteri Pendidikan Nasional (Permendiknas). 
+    Hingga saat ini POLBENG memiliki 8 jurusan yaitu teknik perkapalan, teknik mesin, teknik elektro, teknik sipil, administrasi niaga, teknik informatika, kemaritiman, dan bahasa.
+  """;
+
+  const Home({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amberAccent,
+        backgroundColor: const Color.fromARGB(255, 11, 58, 140),
         title: Text(
-          'Demo Row dan Column',
-          style: GoogleFonts.manrope(fontWeight: FontWeight.w700),
+          'Profil POLBENG',
+          style: GoogleFonts.manrope(
+              fontWeight: FontWeight.w700, color: Colors.white),
         ),
       ),
-      body: Center(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    foregroundColor: Colors.black87,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Image.asset(
+            'images/polbeng_logo.png',
+            height: 245.0,
+            width: 245.0,
+            fit: BoxFit.fill,
+          ),
+          SizedBox(height: 5.0),
+          Row(
+            children: <Widget>[
+              SizedBox(width: 10.0),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    judul,
+                    style: GoogleFonts.manrope(
+                        fontSize: 20.0, fontWeight: FontWeight.bold),
                   ),
-                  child: Text('Button 1',
-                      style: GoogleFonts.pixelifySans(
-                          fontWeight: FontWeight.w600)),
-                  onPressed: () {},
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    foregroundColor: Colors.black87,
+                  Text(
+                    lokasi,
+                    style: GoogleFonts.manrope(
+                      fontSize: 17.0,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  child: Text('Button 2',
-                      style: GoogleFonts.pixelifySans(
-                          fontWeight: FontWeight.w600)),
-                  onPressed: () {},
-                ),
-              ],
+                ],
+              ),
+              SizedBox(width: 15.0),
+              Icon(
+                Icons.star,
+                size: 32.0,
+                color: Colors.red,
+              ),
+              Text(
+                '5',
+                style: GoogleFonts.manrope(fontSize: 18.0),
+              ),
+            ],
+          ),
+          Padding(
+            padding: EdgeInsets.all(10.0),
+            child: Text(
+              deskripsi,
+              style: GoogleFonts.manrope(fontSize: 15.0),
+              softWrap: true,
             ),
-            SizedBox(width: 10),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    foregroundColor: Colors.black87,
-                  ),
-                  child: Text('Button 3',
-                      style: GoogleFonts.pixelifySans(
-                          fontWeight: FontWeight.w600)),
-                  onPressed: () {},
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    foregroundColor: Colors.black87,
-                  ),
-                  child: Text('Button 4',
-                      style: GoogleFonts.pixelifySans(
-                          fontWeight: FontWeight.w600)),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-            SizedBox(width: 10),
-            Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    foregroundColor: Colors.black87,
-                  ),
-                  child: Text('Button 5',
-                      style: GoogleFonts.pixelifySans(
-                          fontWeight: FontWeight.w600)),
-                  onPressed: () {},
-                ),
-                SizedBox(height: 10),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.amberAccent,
-                    foregroundColor: Colors.black87,
-                  ),
-                  child: Text('Button 6',
-                      style: GoogleFonts.pixelifySans(
-                          fontWeight: FontWeight.w600)),
-                  onPressed: () {},
-                ),
-              ],
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
