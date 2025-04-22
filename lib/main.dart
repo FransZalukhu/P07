fae2833c858adb524ee4c909f4f491944007aa37
FransZalukhu <fransdamai2020@gmail.com>
2025-04-22 17:11:11 +0700

    EdgeInsets.all

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 void main() => runApp(MyApp());
 
@@ -9,7 +8,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Implementasi Row & Column',
+      title: 'Jarak Komponen',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -20,75 +19,46 @@ class MyApp extends StatelessWidget {
 }
 
 class Home extends StatelessWidget {
-  final String judul = "Politeknik Negeri Bengkalis";
-  final String lokasi = "Bengkalis, Riau";
-  final String deskripsi = """
-    Politeknik Negeri Bengkalis (POLBENG) adalah satu-satunya politeknik negeri yang berada di Riau. 
-    Pada tanggal 29 Juli 2011, Politeknik Bengkalis resmi menjadi PTN dengan nama Politeknik Negeri Bengkalis melalui Peraturan Menteri Pendidikan Nasional (Permendiknas). 
-    Hingga saat ini POLBENG memiliki 8 jurusan yaitu teknik perkapalan, teknik mesin, teknik elektro, teknik sipil, administrasi niaga, teknik informatika, kemaritiman, dan bahasa.
-  """;
-
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: const Color.fromARGB(255, 11, 58, 140),
-        title: Text(
-          'Profil POLBENG',
-          style: GoogleFonts.manrope(
-              fontWeight: FontWeight.w700, color: Colors.white),
-        ),
+        title: Text('Metode EdgeInsets.all'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
+      body: Row(
         children: <Widget>[
-          Image.asset(
-            'images/polbeng_logo.png',
-            height: 245.0,
-            width: 245.0,
-            fit: BoxFit.fill,
+          Container(
+            color: Colors.lightBlue,
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(3.0),
+            child: ElevatedButton(
+              child: Text('Button 1'),
+              onPressed: () {},
+            ),
           ),
-          SizedBox(height: 5.0),
-          Row(
-            children: <Widget>[
-              SizedBox(width: 10.0),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    judul,
-                    style: GoogleFonts.manrope(
-                        fontSize: 20.0, fontWeight: FontWeight.bold),
-                  ),
-                  Text(
-                    lokasi,
-                    style: GoogleFonts.manrope(
-                      fontSize: 17.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-              SizedBox(width: 15.0),
-              Icon(
-                Icons.star,
-                size: 32.0,
+          Container(
+            margin: const EdgeInsets.all(5.0),
+            padding: const EdgeInsets.all(10.0),
+            decoration: BoxDecoration(
+              border: Border.all(
+                width: 5.0,
                 color: Colors.red,
               ),
-              Text(
-                '5',
-                style: GoogleFonts.manrope(fontSize: 18.0),
-              ),
-            ],
+            ),
+            child: ElevatedButton(
+              child: Text('Button 2'),
+              onPressed: () {},
+            ),
           ),
-          Padding(
-            padding: EdgeInsets.all(10.0),
-            child: Text(
-              deskripsi,
-              style: GoogleFonts.manrope(fontSize: 15.0),
-              softWrap: true,
+          Container(
+            color: Colors.lightBlue,
+            margin: const EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(3.0),
+            child: ElevatedButton(
+              child: Text('Button 3'),
+              onPressed: () {},
             ),
           ),
         ],
